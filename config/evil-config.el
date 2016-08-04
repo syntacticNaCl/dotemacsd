acf1a126620cf1cf3254c605b67c0cbccbe4ec4f
syntacticNaCl <syntacticnacl@dagobah.shootproof.com>
2016-08-04 19:48:48 -0400

    removed old evil commenter shortcuts

--- config/evil-config.el
+++ config/evil-config.el
@@ -23,13 +23,9 @@
   "x" 'delete-window
   "t" 'neotree-toggle
   "ci" 'evilnc-comment-or-uncomment-lines
-  ; "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
-  ; "ll" 'evilnc-quick-comment-or-uncomment-to-the-line
-  ; "cc" 'evilnc-copy-and-comment-lines
-  ; "cp" 'evilnc-comment-or-uncomment-paragraphs
-  ; "cr" 'comment-or-uncomment-region
-  ; "cv" 'evilnc-toggle-invert-comment-line-by-line
-  ; "\\" 'evilnc-comment-operator ; if you prefer backslash key
+  "cc" 'evilnc-copy-and-comment-lines
+  "cp" 'evilnc-comment-or-uncomment-paragraphs
+  "cr" 'comment-or-uncomment-region
   )
 
 (require 'evil-surround)
