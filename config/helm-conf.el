ff8bd4057a9687ff215f7405e0b265cf4ff0ebbd
Garrett Rappaport <garrettrappaport@gmail.com>
2016-09-28 15:55:53 -0400

    added tab completion in helm buffer and updated key bindings

--- config/helm-conf.el
+++ config/helm-conf.el
@@ -1,7 +1,14 @@
 (require 'helm-config)
 
+;; allow for tab completion in helm buffer
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x m") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 (global-set-key (kbd "C-x r b") 'helm-bookmarks)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+
 
 (provide 'helm-conf)
